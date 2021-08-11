b91f9e7adb6460a76694b907e7bcbaf6b89cd0e7
Kesinee Powers <56649145+KesineeP@users.noreply.github.com>
2021-08-11 11:43:55 -0400

    Reinstalled express-generator and clean up code

--- app.js
+++ app.js
@@ -1,14 +1,43 @@
-const express = require("express");
+const createError = require('http-errors');
+const express = require('express');
+// const path = require('path');
+// const cookieParser = require('cookie-parser');
+// const logger = require('morgan');
+
+const notifymeRouter = require('./routes/notifyme');
+const testRouter = require('./routes/test');
+
 const app = express();
-const port = 3000;
-const test = require("./routes/test");
-const notifyme = require("./routes/notifyme");
+const port = 3000
 
+
+
+// app.use(logger('dev'));
 app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
 
-app.use("/", test);
-app.use("/notifyme", notifyme);
+app.use('/', testRouter);
+app.use('/notifyme', notifymeRouter);
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+})
+
+// module.exports = app;
