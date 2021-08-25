f188727572c9a9bacaed4565889e51b5a3fe275c
Kesinee Powers <56649145+KesineeP@users.noreply.github.com>
2021-08-24 22:05:39 -0400

    Deleted unused files and comments

--- app.js
+++ app.js
@@ -23,11 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/test', testRouter);
 app.use('/notifyme', notifymeRouter);
-
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/notifyme.js
+++ routes/notifyme.js
@@ -6,10 +6,8 @@ const pool = require('../db')
 router.post("/", async (req, res) => {
   try {
     const {email} = req.body;
-    const addEmail = await pool.query(`INSERT INTO "email-service".email_list (email) VALUES ($1)`, [email])
-    // const emailList = await pool.query(`SELECT email
-    // FROM "email-service".email_list;`);
-    // await pool.end();
+    await pool.query(`INSERT INTO "email-service".email_list (email) VALUES ($1)`, [email])
+    
   }
   catch (err) {
     console.error(err.message);
