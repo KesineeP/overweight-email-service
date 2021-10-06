0745478816a69f26c4791588d322906731cbbc5e
Kesinee Powers <56649145+KesineeP@users.noreply.github.com>
2021-10-05 22:13:26 -0400

    Debugginh unsubscribe

--- routes/unsubscribe.js
+++ routes/unsubscribe.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const pool = require("../db");
 
 router.delete("/", async (res, req) => {
-  const { email } = res.body;
   try {
     await pool.query(
       `DELETE
