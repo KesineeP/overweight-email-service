5be11f2d43fc04389e886f118db2cc30313d3fb0
Kesinee Powers <56649145+KesineeP@users.noreply.github.com>
2021-10-07 23:14:19 -0400

    Checking If User has already unsubscribe

--- routes/unsubscribe.js
+++ routes/unsubscribe.js
@@ -2,17 +2,24 @@ const express = require("express");
 const router = express.Router();
 const pool = require("../db");
 
-router.delete("/:email", async (res, req) => {
+router.delete("/:email", async (res, req, next) => {
   const { email } = res.params;
-  console.log(email);
+
   try {
-    await pool.query(
+    const unsubscribe = await pool.query(
       `DELETE
             FROM "email-service".email_list
             WHERE email = '${email}'
       `
     );
-    res.send(`${email} Succesfully Unsubscribed`);
+    //if rowCount === 0
+    //send back message say "${email} already unsubscribe"
+    //else send back message say "${email} Successfully Unsubscribe"
+    if (unsubscribe.rowCount === 0) {
+      console.log(`${email} Already Unsubscribed`);
+    } else {
+      console.log(`${email} Successfully Unsubscribed`);
+    }
   } catch (err) {
     console.log(err);
   }
